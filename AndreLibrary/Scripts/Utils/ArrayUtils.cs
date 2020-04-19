f62fdda2e3de4c354fa9eee36fa50fc448cf3e48
Andre <andregot@gmail.com>
2020-04-19 15:13:07 +1200

    added array and math utils

---
+++ AndreLibrary/Scripts/Utils/ArrayUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Andre.Utils
+{
+    public static class ArrayUtils 
+    {
+        public static int ArrayIndex2DTo1D(int x, int y, int width)
+        {
+            return x + (width * y);
+        }
+
+        public static Vector2Int ArrayPositionDTo1D(int i, int width)
+        {
+            return new Vector2Int(i% width, i/ width);
+        }
+
+        public static int ArrayIndex3DTo1D(int x, int y, int z, int width, int height)
+        {
+            return x + (width * y) + (width*height*z);
+        }
+
+        public static Vector3Int ArrayPosition3DTo1D(int i, int width, int height)
+        {
+            return new Vector3Int(i % width, (i / width)% height, i/(width*height));
+        }
+    }
+}
---
+++ AndreLibrary/Scripts/Utils/MathUtils.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Andre.Utils
+{
+    public static class MathUtils 
+    {
+        public static float RandomEqualChanceFloat(float min,float max)
+        {
+            if (min >= max)
+                return 0;
+            return min + (Random.value * ((max - min)- 0.01f));
+        }
+
+        public static float RandomEqualChanceFloat(float max)
+        {
+            return RandomEqualChanceFloat(0, max);
+        }
+
+        public static float RandomEqualChanceFloat(int min, int max)
+        {
+            float maxTmp = max;
+            float minTmp = min;
+            return RandomEqualChanceFloat(minTmp, maxTmp);
+        }
+
+        public static float RandomEqualChanceFloat(int max)
+        {
+            float maxTmp = max;
+            return RandomEqualChanceFloat(0, maxTmp);
+        }
+
+        public static int RandomEqualChanceInt(float min, float max)
+        {
+            return Mathf.FloorToInt(RandomEqualChanceFloat(min, max));
+        }
+
+        public static int RandomEqualChanceInt(float max)
+        {
+            return Mathf.FloorToInt(RandomEqualChanceFloat(0, max));
+        }
+
+        public static int RandomEqualChanceInt(int min, int max)
+        {
+            float maxTmp = max;
+            float minTmp = min;
+            return Mathf.FloorToInt(RandomEqualChanceFloat(minTmp, maxTmp));
+        }
+
+        public static int RandomEqualChanceInt(int max)
+        {
+            float maxTmp = max;
+            return Mathf.FloorToInt(RandomEqualChanceFloat(0, maxTmp));
+        }
+    }
+}
